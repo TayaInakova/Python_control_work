91f8ff50cbf0aed2f90ed7277cd437c4ba56a726
Taya Inakova <punkgrrl@yandex.ru>
2023-11-12 05:42:17 +0300

    view note done

--- notes/view_notes.py
+++ notes/view_notes.py
@@ -1,11 +1,35 @@
 def all_notes_view(notes):
-    print()
+    for note in notes:
+        print("№", note["ID"], note["Date"], note["Title"])
+    note_text_view(notes)
 
 
 def note_text_view(notes):
-    print()
+    num = input('Введите номер записи для чтения заметки или "0" для выхода из режима чтения заметок: ')
+    try:
+        number = int(num)
+        if number != 0 and number <= len(notes):
+            for note in notes:
+                if note["ID"] == number:
+                    print(note["Text"])
+                    break
+            note_text_view(notes)
+        elif number == 0:
+            _ = input('Введите любой символ для возвращения в меню: ')
+        else:
+            print("нет записи с таким номером")
+            note_text_view(notes)
+    except Exception:
+        print("нет записи с таким номером")
 
 
 def view_sorted_notes(notes):
-    print()
+    day = input("укажите дату в формате дд.мм.гггг:\n")
+    filter_list = []
+    for note in notes:
+        if day in note['Date']:
+            filter_list.append(note)
+            print("№", note["ID"], note["Date"], note["Title"])
+            print()
+    note_text_view(notes)
     return notes
