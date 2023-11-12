024c6913ecf90f0b73e4a6a261d785a1419958c8
Taya Inakova <punkgrrl@yandex.ru>
2023-11-12 05:45:09 +0300

    change note done

--- notes/change_note.py
+++ notes/change_note.py
@@ -1,3 +1,16 @@
+import add_note
+
+
 def change_note(notes):
-    print()
+    changed = int(
+        input('Введите номер записи, которую хотите изменить: '))
+    try:
+        for note in notes:
+            if note["ID"] == changed:
+                notes.remove(note)
+                notes.append(add_note.create_new_note(changed))
+                print("Запись изменена!")
+        _ = input('Введите любой символ для возвращения в меню: ')
+    except Exception:
+        print('Нет записи с таким номером.')
     return notes
