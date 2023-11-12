90f34cf51010398d904b88e4be79c491c2631bdd
Taya Inakova <punkgrrl@yandex.ru>
2023-11-12 05:40:42 +0300

    add note done

--- notes/add_note.py
+++ notes/add_note.py
@@ -1,3 +1,18 @@
+import datetime
+import json
+import random
+
+
+def create_new_note(id):
+    note = {"ID": id,
+            "Date": datetime.datetime.today().strftime("%d.%m.%Y  %H:%M"),
+            "Title": input("Введите заголовок заметки:\n"),
+            "Text": input("Введите текст заметки:\n")}
+    json.dumps(note, ensure_ascii=False)
+    return note
+
+
 def new_note(notes):
-    print()
+    notes.append(create_new_note(int(random.random()*1000)))
+    print("Заметка сохранена!")
     return notes
