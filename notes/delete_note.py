a55c195464012cabffc6a7da0c31a4bc2e7b7eb2
Taya Inakova <punkgrrl@yandex.ru>
2023-11-12 05:43:49 +0300

    delete note done

--- notes/delete_note.py
+++ notes/delete_note.py
@@ -1,3 +1,12 @@
 def delete_note_in_notes(notes):
-    print()
+    deleted = int(
+        input('Введите номер записи, которую хотите удалить: '))
+    try:
+        for note in notes:
+            if note["ID"] == deleted:
+                notes.remove(note)
+        print("Запись удалена!")
+        _ = input('Введите любой символ для возвращения в меню: ')
+    except Exception:
+        print('Нет записи с таким номером.')
     return notes
